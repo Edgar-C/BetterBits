33ec752bb6ad5099df58de035c3fe8e2deadb300
Edgar-C <49798068+Edgar-C@users.noreply.github.com>
2019-12-09 07:23:44 -0500

    Update addadr.php

--- addadr.php
+++ addadr.php
@@ -1,14 +1,8 @@
 <?php
 session_start();
-#require("php_includes/check_login_status.php");
-#if($user_ok == false) {
-#echo "broke";
-#exit();
-#}
 $file97 = './boomers';
 $du = $_SESSION['username'];
 file_put_contents($file97,$du);
 header("location: 1sec.php"); 
-#echo $du;
 exit();
 ?>
